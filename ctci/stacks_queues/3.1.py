769a624bb33f9452aae741451571507f43be8fcb
Ivan P <21068310+ihlpo@users.noreply.github.com>
2022-08-18 22:15:26 +1000

    stack and queues

---
+++ ctci/stacks_queues/3.1.py
@@ -0,0 +1,9 @@
+class Stack:
+    def __init__(self):
+        self.array = [None for i in range(9)]]
+
+x = Stack()
+x.stack1[1] = 1
+print(x.stack1)                  
+print(x.array)
+print(x.stack1)                  
